1d4e0f842c91d0ead945c8b773413be79327ed68
Erinberht <eribarher203@gmail.com>
2025-06-17 12:30:31 -0500

    configure mi proyecto para que se conecete al mi backend

--- src/composables/api.ts
+++ src/composables/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://backend-barberiaelite-production.up.railway.app/api',
   withCredentials: true,
   headers: {
     Accept: 'application/json',
